4308487fd84a49e63890ef01dde2b2daf460c918
prathmesh naik <32015484+kingofcars3711@users.noreply.github.com>
2021-04-29 15:39:58 +0530

    main

---
+++ Main.java
@@ -0,0 +1,29 @@
+package isa2jenkins;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter two numbers");
+	    int No1 = 87;
+	    int No2 =23 ;
+	    
+	    System.out.println(No1 + " " + No2);
+
+	    // Add two numbers
+	    int sum = No1 + No2;
+	    System.out.println("Sum of two no: " + sum);
+	    
+	    // Difference of two numbers
+	    int difference = No1 - No2;
+	    System.out.println("Difference of two:" + difference);
+	    
+	    // Multiplication of two numbers
+	    int multiplication = No1 - No2;
+	    System.out.println("Multiplication answer is: " + multiplication);
+	    
+
+	
+	}
+
+}
